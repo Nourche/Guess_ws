94681ad76f83dfc5ee9d5dfc26a85fcdbc69eb39
NourChe <nour3che@gmail.com>
2023-12-06 20:52:49 +0000

    tkhlita

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const WebSocket = require('ws');
+const http = require('http');
+const fs = require('fs');
 
-const server = new WebSocket.Server({ port: 8081 });
+// Create an HTTP server to serve the HTML page
+const server = http.createServer((req, res) => {
+    fs.readFile('index.html', 'utf8', (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        }
+    });
+});
 
-server.on('connection', (socket) => {
+// Create a WebSocket server using the HTTP server
+const wss = new WebSocket.Server({ server });
+
+// Event listener for when a client connects to the WebSocket server
+wss.on('connection', (socket) => {
     console.log('Client connected');
 
     // Send a welcome message to the client
@@ -17,4 +34,9 @@ server.on('connection', (socket) => {
     socket.on('close', () => {
         console.log('Client disconnected');
     });
+});
+
+// Start the server on port 8081
+server.listen(8081, () => {
+    console.log('Server listening on http://localhost:8081');
 });
